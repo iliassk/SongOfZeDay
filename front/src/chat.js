1686aa922422a4e1ab55d13c68cccc2da428a635
iliassk <kenfaoui.iliass@gmail.com>
2018-11-04 22:34:51 +0100

    Upgrading to Angular 7 and Angular CLI

--- front/src/chat.js
+++
@@ -1,7 +0,0 @@
-import people from "./people.js";
-
-export function init() {
-  const root = document.createElement("div");
-  root.innerHTML = `<p>There'rre ${people.length} people in the room.</p>`;
-  document.body.appendChild(root);
-}
