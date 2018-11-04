4063319ccfe0c0a1a6663671ef41a0daa5380f2d
iliassk <kenfaoui.iliass@gmail.com>
2018-11-04 22:49:11 +0100

    Initial back commit

--- back/src/main/java/com/iliass/projects/songofzedayback/SongOfZeDayBackApplication.java
+++ back/src/main/java/com/iliass/projects/songofzedayback/SongOfZeDayBackApplication.java
@@ -1,4 +1,4 @@
-package com.iliass.projects.songofzedayback.songofzedayback;
+package com.iliass.projects.songofzedayback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- back/src/test/java/com/iliass/projects/songofzedayback/SongOfZeDayBackApplicationTests.java
+++ back/src/test/java/com/iliass/projects/songofzedayback/SongOfZeDayBackApplicationTests.java
@@ -1,4 +1,4 @@
-package com.iliass.projects.songofzedayback.songofzedayback;
+package com.iliass.projects.songofzedayback;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
