ff4316e01260adb9b67df38a9bad2112c5286738
iliassk <kenfaoui.iliass@gmail.com>
2018-11-04 22:47:38 +0100

    Initial back commit

---
+++ back/src/main/java/com/iliass/projects/songofzedayback/songofzedayback/SongOfZeDayBackApplication.java
@@ -0,0 +1,12 @@
+package com.iliass.projects.songofzedayback.songofzedayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongOfZeDayBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongOfZeDayBackApplication.class, args);
+	}
+}
