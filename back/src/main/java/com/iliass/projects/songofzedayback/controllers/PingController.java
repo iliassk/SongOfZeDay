f5a088583f9a98a28f3d9725179e5c209ccbaaf7
iliassk <kenfaoui.iliass@gmail.com>
2018-11-13 23:53:31 +0100

    Configuring logger

--- back/src/main/java/com/iliass/projects/songofzedayback/controllers/PingController.java
+++ back/src/main/java/com/iliass/projects/songofzedayback/controllers/PingController.java
@@ -1,5 +1,7 @@
 package com.iliass.projects.songofzedayback.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,15 @@ import java.util.Map;
 @RestController
 public class PingController {
 
+    private Logger logger;
+
+    public PingController() {
+        logger = LoggerFactory.getLogger(PingController.class);
+    }
+
     @GetMapping(value = "/ping", produces = MediaType.APPLICATION_JSON_VALUE)
     public Map getSignal() {
+        logger.info("Pinging the backend");
         return Collections.singletonMap("response", "I'm ALIVE");
     }
 }
